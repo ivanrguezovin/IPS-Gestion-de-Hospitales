b609f9199ce722ee160a77e5d130a760f3569c39
Alejandro Sáez Morollón <uo198755@uniovi.es>
2019-12-01 18:51:47 +0100

    Add logs for critical actions
    Changes to be committed:
    	modified:   src/main/java/es/uniovi/ips/hospital/service/AdminAssistantService.java
    	modified:   src/main/java/es/uniovi/ips/hospital/service/AppointmentService.java
    	modified:   src/main/java/es/uniovi/ips/hospital/service/DiagnosticService.java
    	modified:   src/main/java/es/uniovi/ips/hospital/service/DoctorService.java
    	modified:   src/main/java/es/uniovi/ips/hospital/service/LoginService.java
    	modified:   src/main/java/es/uniovi/ips/hospital/service/MedicalRecordService.java
    	modified:   src/main/java/es/uniovi/ips/hospital/service/NurseService.java
    	modified:   src/main/java/es/uniovi/ips/hospital/service/PatientService.java
    	modified:   src/main/java/es/uniovi/ips/hospital/service/RoomService.java
    	modified:   src/main/java/es/uniovi/ips/hospital/service/ScheduleService.java
    	modified:   src/main/java/es/uniovi/ips/hospital/service/VaccineService.java
    	modified:   src/main/java/es/uniovi/ips/hospital/util/security/SecurityConfig.java
    	modified:   src/main/resources/application.yaml

--- src/main/java/es/uniovi/ips/hospital/service/MedicalRecordService.java
+++ src/main/java/es/uniovi/ips/hospital/service/MedicalRecordService.java
@@ -1,32 +1,40 @@
 package es.uniovi.ips.hospital.service;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import es.uniovi.ips.hospital.domain.MedicalRecord;
 import es.uniovi.ips.hospital.domain.Patient;
 import es.uniovi.ips.hospital.repository.MedicalRecordRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service("MedicalRecordService")
 public class MedicalRecordService {
-	
-	@Autowired
-	private MedicalRecordRepository medicalRecordRepository;
+
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private MedicalRecordRepository medicalRecordRepository;
 
     public MedicalRecord createMedicalRecord(MedicalRecord medicalRecord) {
         medicalRecordRepository.save(medicalRecord);
-        return medicalRecordRepository.findByPatientAndDateAndDescriptionAndPrescription(medicalRecord.getPatient(), medicalRecord.getDate(),
-        			medicalRecord.getDescription(), medicalRecord.getPrescription());
+		log.info("a medical record for the patient " + medicalRecord.getPatient().getEmail() + " has been created");
+        return medicalRecordRepository.findByPatientAndDateAndDescriptionAndPrescription(medicalRecord.getPatient(),
+				medicalRecord.getDate(),
+                medicalRecord.getDescription(),
+				medicalRecord.getPrescription());
     }
-    
+
     public MedicalRecord findByPatient(Patient patient) {
-		return medicalRecordRepository.findByPatient(patient);
-	}
-    
+    	log.info("the medical record of the patient " + patient.getEmail() + " has been retrieved");
+        return medicalRecordRepository.findByPatient(patient);
+    }
+
     public List<MedicalRecord> findAllByPatient(Patient patient) {
-		return medicalRecordRepository.findAllByPatient(patient);
-	}
+		log.info("the list of medical records of the patient " + patient.getEmail() + " has been retrieved");
+        return medicalRecordRepository.findAllByPatient(patient);
+    }
 
 }
